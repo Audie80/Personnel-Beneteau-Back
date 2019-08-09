3359e011ac2e32068d30d51338e2eafc791d9f77
Audie80 <audie.menut@gmail.com>
2019-08-09 11:28:30 +0200

    changement de port

--- app.js
+++ app.js
@@ -30,6 +30,6 @@ bddsql.BDDSQL.connect(function (err) {
 
 
 // listen for requests
-app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+app.listen(8080, () => {
+    console.log("Server is listening on port 8080");
 });
